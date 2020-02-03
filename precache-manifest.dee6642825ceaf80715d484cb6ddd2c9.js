70d8f056c4c6f6ebc924fe67eac8c585714685c2
Inna <inna.tereshchenko1@gmail.com>
2020-02-03 16:37:14 +0200

    Updates

---
+++ precache-manifest.dee6642825ceaf80715d484cb6ddd2c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bd044d7b8b536e1788adf43dea92a8",
+    "url": "/audio_widget/index.html"
+  },
+  {
+    "revision": "e82f09e1c93c3ababa35",
+    "url": "/audio_widget/static/css/main.3dabafc6.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/audio_widget/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "e82f09e1c93c3ababa35",
+    "url": "/audio_widget/static/js/main.6e847804.chunk.js"
+  },
+  {
+    "revision": "04482fe5b5f1287af0d8",
+    "url": "/audio_widget/static/js/runtime~main.ffbaa799.js"
+  }
+]);
