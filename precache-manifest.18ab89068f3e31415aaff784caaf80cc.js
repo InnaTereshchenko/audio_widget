6ac8ef80630ca0f37dc6b186ddcd059730483d16
Inna <inna.tereshchenko1@gmail.com>
2020-02-05 15:01:46 +0200

    Updates

--- precache-manifest.18ab89068f3e31415aaff784caaf80cc.js
+++ precache-manifest.18ab89068f3e31415aaff784caaf80cc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21bcc8ddaf1ed3196e953bab201c9dd6",
+    "revision": "744ba0e16ba058cb03bf9e9120041f9c",
     "url": "/audio_widget/index.html"
   },
   {
-    "revision": "75a174a04b566b5b1a29",
-    "url": "/audio_widget/static/css/main.b44ff96f.chunk.css"
+    "revision": "8874c1d4d38d2348c3b5",
+    "url": "/audio_widget/static/css/main.44bd4ad3.chunk.css"
   },
   {
     "revision": "08ac458e926d86e508a3",
     "url": "/audio_widget/static/js/2.61611653.chunk.js"
   },
   {
-    "revision": "75a174a04b566b5b1a29",
-    "url": "/audio_widget/static/js/main.9e55b1b2.chunk.js"
+    "revision": "8874c1d4d38d2348c3b5",
+    "url": "/audio_widget/static/js/main.f2859678.chunk.js"
   },
   {
     "revision": "04482fe5b5f1287af0d8",
